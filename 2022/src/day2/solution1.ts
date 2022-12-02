7e12aadb636d68d03f29dca5eeaf03eb573a6150
Ozoku <19756856+ozokuz@users.noreply.github.com>
2022-12-02 21:35:58 +0200

    better solution to day 2 part 1

--- 2022/src/day2/solution1.ts
+++ 2022/src/day2/solution1.ts
@@ -7,28 +7,35 @@ const inputData = await fs.readFile(
   "utf-8"
 );
 
-type OL = "A" | "B" | "C";
-type RL = "X" | "Y" | "Z";
-
 const scores = {
-  AX: 4,
-  AY: 8,
-  AZ: 3,
-  BX: 1,
-  BY: 5,
-  BZ: 9,
-  CX: 7,
-  CY: 2,
-  CZ: 6,
+  A: 1,
+  B: 2,
+  C: 3,
+  X: 1,
+  Y: 2,
+  Z: 3,
+} as const;
+
+const results = {
+  [-1]: 6,
+  [2]: 6,
+  [0]: 3,
+  [-2]: 0,
+  [1]: 0,
 } as const;
 
 const lines = inputData.split("\n");
 lines.pop();
 
-const rounds = lines.map((r) => r.split(" ") as [OL, RL]);
+const rounds = lines.map(
+  (r) => r.split(" ") as [keyof typeof scores, keyof typeof scores]
+);
 
 const scoresPerRound = rounds.map(
-  ([opponent, recommendation]) => scores[`${opponent}${recommendation}`]
+  ([opponent, recommendation]) =>
+    results[
+      (scores[opponent] - scores[recommendation]) as keyof typeof results
+    ] + scores[recommendation]
 );
 
 const total = scoresPerRound.reduce((acc, current) => acc + current, 0);
