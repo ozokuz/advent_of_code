1045c22fd01a9da5c69362811af5792aa0419df0
Ozoku <19756856+ozokuz@users.noreply.github.com>
2022-12-02 23:06:28 +0200

    more better solution to day 2 part 1

--- 2022/src/day2/solution1.ts
+++ 2022/src/day2/solution1.ts
@@ -7,35 +7,25 @@ const inputData = await fs.readFile(
   "utf-8"
 );
 
-const scores = {
-  A: 1,
-  B: 2,
-  C: 3,
-  X: 1,
-  Y: 2,
-  Z: 3,
-} as const;
+const scores = { A: 1, B: 2, C: 3, X: 1, Y: 2, Z: 3 } as const;
 
-const results = {
-  [-1]: 6,
-  [2]: 6,
-  [0]: 3,
-  [-2]: 0,
-  [1]: 0,
-} as const;
+const winScores = [-1, 2];
+
+const getOutcomeScore = (v: number) => {
+  if (v === 0) return 3;
+  if (winScores.includes(v)) return 6;
+  return 0;
+};
 
 const lines = inputData.split("\n");
 lines.pop();
 
-const rounds = lines.map(
-  (r) => r.split(" ") as [keyof typeof scores, keyof typeof scores]
+const rounds = lines.map((r) =>
+  r.split(" ").map((v) => scores[v as keyof typeof scores])
 );
 
 const scoresPerRound = rounds.map(
-  ([opponent, recommendation]) =>
-    results[
-      (scores[opponent] - scores[recommendation]) as keyof typeof results
-    ] + scores[recommendation]
+  (round) => getOutcomeScore(round[0]! - round[1]!) + round[1]!
 );
 
 const total = scoresPerRound.reduce((acc, current) => acc + current, 0);
